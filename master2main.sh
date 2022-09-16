53c72c3a1611a31b2fc46110a857359a7cb6e453
Nathan DiMauro <nathan.dimauro@asurion.com>
2022-09-16 15:37:55 -0400

    updated m2m scripts

--- master2main.sh
+++ master2main.sh
@@ -4,7 +4,7 @@ echo creating branch main from master
 git branch -m master main
 git push -u origin main
 
-
-echo $'\e[36mGo to github settings -> branches and swap default branch to master\e[0m'
+link=`git remote -v | head -n 1 | sed 's/^[^http]*//' | sed 's/.git (fetch)/\/settings\/branches/'`
+echo $'Go to\e[36m' $link $'\e[0mand swap default branch to master'
 read -n 1 -p "Press any key to continue:" junk
 git push origin --delete master
